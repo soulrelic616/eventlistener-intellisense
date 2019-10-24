113b0589840affcdb8f249cae7d4d81764a152d8
soulrelic616 <fataldesign616@gmail.com>
2019-10-24 13:50:58 +0100

    starts creating function for dynamic quotemarks

--- extension.js
+++ extension.js
@@ -102,17 +102,38 @@ function activate(context) {
 	var CompletionItems = [];
 	var CompletionItem;
 
+	//var quoteMark = new RegExp(/["\']/g);
+
 	function loopListeners(){
 		eventListeners.forEach(function(item, index){
-			console.log(item);
+			//console.log(item);
 			CompletionItem = new vscode.CompletionItem(item, vscode.CompletionItemKind.Class);
 			CompletionItems.push(CompletionItem);
 
-			console.log(CompletionItems)
+			//console.log(CompletionItems)
 		});
 	}
 
 	loopListeners();
+
+
+	var quoteMark;
+
+	vscode.workspace.onDidChangeTextDocument(changeEvent => {
+		for (const change of changeEvent.contentChanges) {
+			 //console.log(change.range); // range of text being replaced
+			 //console.log(change.text); // text replacement
+
+			if(change.text == '"'){
+				quoteMark = '"';
+			} else if (change.text == "'"){
+				quoteMark = "'";
+			} else{
+				quoteMark = undefined;
+			}
+			console.log(quoteMark);
+		}
+   });
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
