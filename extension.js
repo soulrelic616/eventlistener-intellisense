ddaa3462277e19fd529e9fd2b5d5bba77667fad5
soulrelic616 <fataldesign616@gmail.com>
2019-10-24 13:33:53 +0100

    function starts to sort of work

--- extension.js
+++ extension.js
@@ -10,7 +10,7 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	const eventListeners = [
+	let eventListeners = [
 		"Event",
 		"abort",
 		"afterprint",
@@ -99,6 +99,21 @@ function activate(context) {
 		"wheel",
 	];
 
+	var CompletionItems = [];
+	var CompletionItem;
+
+	function loopListeners(){
+		eventListeners.forEach(function(item, index){
+			console.log(item);
+			CompletionItem = new vscode.CompletionItem(item, vscode.CompletionItemKind.Class);
+			CompletionItems.push(CompletionItem);
+
+			console.log(CompletionItems)
+		});
+	}
+
+	loopListeners();
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "eventlistener-intellisense" is now active!');
@@ -117,20 +132,10 @@ function activate(context) {
 	let provider1 = vscode.languages.registerCompletionItemProvider('javascript', {
 
         provideCompletionItems(document, position, token, context) {
-            // a simple completion item which inserts `Hello World!`
-            const simpleCompletion = new vscode.CompletionItem('Hello World!');
-            // a completion item that inserts its text as snippet,
-            // the `insertText`-property is a `SnippetString` which will be
-            // honored by the editor.
-            const snippetCompletion = new vscode.CompletionItem('Good part of the day');
-            snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
-            snippetCompletion.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
-            // a completion item that can be accepted by a commit character,
-            // the `commitCharacters`-property is set which means that the completion will
-            // be inserted and then the character will be typed.
-            const commitCharacterCompletion = new vscode.CompletionItem('testing');
-            commitCharacterCompletion.commitCharacters = ['.'];
-            commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `testing.`');
+            
+            const commitCharacterCompletion = new vscode.CompletionItem("on(");
+            commitCharacterCompletion.commitCharacters = ["'"];
+            commitCharacterCompletion.documentation = new vscode.MarkdownString("Press `'` to get `lol`");
             // a completion item that retriggers IntelliSense when being accepted,
             // the `command`-property is set which the editor will execute after 
             // completion has been inserted. Also, the `insertText` is set so that 
@@ -141,8 +146,7 @@ function activate(context) {
             commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
             // return all completion items as array
             return [
-                simpleCompletion,
-                snippetCompletion,
+               
                 commitCharacterCompletion,
                 commandCompletion
             ];
@@ -155,16 +159,17 @@ function activate(context) {
             // get all text until the `position` and check if it reads `console.`
             // and if so then complete if `log`, `warn`, and `error`
             let linePrefix = document.lineAt(position).text.substr(0, position.character);
-            if (!linePrefix.endsWith('testing.')) {
+            if (!linePrefix.endsWith("on('")) {
                 return undefined;
             }
             return [
-                new vscode.CompletionItem('log', vscode.CompletionItemKind.Class),
+                /* new vscode.CompletionItem('log', vscode.CompletionItemKind.Class),
                 new vscode.CompletionItem('warn', vscode.CompletionItemKind.Class),
-                new vscode.CompletionItem('error', vscode.CompletionItemKind.Class),
+				new vscode.CompletionItem('error', vscode.CompletionItemKind.Class), */
+				CompletionItem
             ];
         }
-    }, '.'); // triggered whenever a '.' is being typed
+    }, "'"); // triggered whenever a '.' is being typed
 
 	context.subscriptions.push(provider1, provider2, disposable); 
 }
